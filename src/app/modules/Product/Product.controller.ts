829e8f0c9192a1bbc37209af9ebfeb491b2de480
gsjoy24 <gour.joy24@gmail.com>
2024-03-10 00:07:32 +0600

    Refactor ProductController and import dependencies properly

--- src/app/modules/Product/Product.controller.ts
+++ src/app/modules/Product/Product.controller.ts
@@ -1,7 +1,10 @@
-import httpStatus from "http-status";
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import ProductServices from './Product.service';
 
 const addProduct = catchAsync(async (req, res) => {
-  const result = await ProductService.addProduct(req.body);
+  const result = await ProductServices.addProduct(req.body);
 
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
@@ -10,4 +13,60 @@ const addProduct = catchAsync(async (req, res) => {
     data: result,
   });
 });
- 
+
+const getProducts = catchAsync(async (req, res) => {
+  const result = await ProductServices.getProducts(req.query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Products are fetched successfully',
+    data: result,
+  });
+});
+
+const getProductById = catchAsync(async (req, res) => {
+  const result = await ProductServices.getProductById(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product is fetched successfully',
+    data: result,
+  });
+});
+
+const updateProductById = catchAsync(async (req, res) => {
+  const result = await ProductServices.updateProductById(
+    req.params.id,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product is updated successfully',
+    data: result,
+  });
+});
+
+const deleteProductById = catchAsync(async (req, res) => {
+  const result = await ProductServices.deleteProductById(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Product is deleted successfully',
+    data: result,
+  });
+});
+
+const ProductController = {
+  addProduct,
+  getProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+};
+
+export default ProductController;
