937a8081af42e40e6888b800ae17d2716f238c10
gsjoy24 <gour.joy24@gmail.com>
2024-03-09 23:08:32 +0600

    Fix bug in login functionality

---
+++ src/app/modules/Product/Product.type.ts
@@ -0,0 +1,18 @@
+type TInfo = {
+  title: string;
+  description: string;
+};
+export type TProduct = {
+  _id?: string;
+  name: string;
+  description: string;
+  additional_info: TInfo[];
+  old_price: number;
+  last_price: number;
+  stock: number;
+  primary_image: string;
+  images: string[];
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
