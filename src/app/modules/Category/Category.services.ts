f95bbad250cf20c1e6a665c746908645105f6a24
gsjoy24 <gour.joy24@gmail.com>
2024-03-12 18:05:09 +0600

    Add CRUD operations for Category module

--- src/app/modules/Category/Category.services.ts
+++ src/app/modules/Category/Category.services.ts
@@ -14,3 +14,41 @@ const CreateCategory = async (data: TCategory) => {
   const category = await Category.create(data);
   return category;
 };
+
+const GetCategories = async () => {
+  const categories = await Category.find();
+  return categories;
+};
+
+const GetCategoryById = async (id: string) => {
+  const category = await Category.findById(id);
+  if (!category) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Category not found');
+  }
+  return category;
+};
+
+
+const UpdateCategoryById = async (id : string, data: Partial<TCategory>) => {
+   const category = await Category.findByIdAndUpdate(id, data, { new: true }); 
+   if (!category) {
+      throw new AppError(httpStatus.NOT_FOUND, 'Category not found');
+   }
+   return category; 
+}
+
+const DeleteCategoryById = async (id : string) => { 
+   const category = await Category.findByIdAndDelete(id); 
+   if (!category) {
+      throw new AppError(httpStatus.NOT_FOUND, 'Category not found');
+   }
+   return category; 
+}
+
+export default {
+  CreateCategory,
+  GetCategories,
+  GetCategoryById,
+  UpdateCategoryById,
+  DeleteCategoryById,
+};
