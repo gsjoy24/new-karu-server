a4d973860e3086e4facfe08ab695c3fbe06cbb17
gsjoy24 <gour.joy24@gmail.com>
2024-04-23 12:53:47 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const app: Application = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3006',
+  'http://localhost:3000',
   'https://karukon.com',
 ];
 app.use(
